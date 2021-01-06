e332e948bca5a68b073c8d5d0bb04b3dd557bfc6
Kevin Yu <thekevinyu@gmail.com>
2021-01-05 21:11:16 -0800

    Bump version #

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name = "inspec",
-    version = "0.1",
+    version = "0.2",
     packages = [
         "inspec",
         "inspec.gui",
