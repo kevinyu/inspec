A couple extra options for scaling transforms and bug fix in maps

inspec/maps.py
@@ -59,7 +59,7 @@ class CharMap(object):
         return (char_rows * cls.patch_dimensions[0], char_cols * cls.patch_dimensions[1])
 
     @classmethod
-    def to_char_array(cls, img):
+    def to_char_array(cls, img, floor=None, ceil=None):
         """Convert an image array into an array of tuples with character and color info
 
         Params
@@ -77,8 +77,10 @@ class CharMap(object):
 
         # img_min = np.min(img)
         # img_max = np.max(img)
-        floor = np.quantile(img, var.SPECTROGRAM_LOWER_QUANTILE)
-        ceil = np.quantile(img, var.SPECTROGRAM_UPPER_QUANTILE)
+        if floor is None:
+            floor = np.quantile(img, var.SPECTROGRAM_LOWER_QUANTILE)
+        if ceil is None:
+            ceil = np.quantile(img, var.SPECTROGRAM_UPPER_QUANTILE)
 
         output_shape = (
             img.shape[0] // cls.patch_dimensions[0],
@@ -88,8 +90,9 @@ class CharMap(object):
         char_array = np.empty(output_shape, dtype=object)
         for (row, col), patch in cls.iter_patches(img):
             if floor == ceil:
-                return cls.background_char
-            char_array[row, col] = cls.patch_to_char((patch - floor) / (ceil - floor))
+                char_array[row, col] = cls.background_char
+            else:
+                char_array[row, col] = cls.patch_to_char((patch - floor) / (ceil - floor))
 
         return char_array
 

inspec/transform.py
@@ -245,7 +245,7 @@ class AmplitudeEnvelopeTwoSidedTransform(AudioTransform):
         self.ymax = ymax
         self.gradient = gradient
 
-    def convert(self, data, sampling_rate, output_size):
+    def convert(self, data, sampling_rate, output_size, scale=1.0):
         """Convert 1D audio signal into a spectrogram
 
         Returns spectrogram and metadata dictionary
@@ -263,6 +263,8 @@ class AmplitudeEnvelopeTwoSidedTransform(AudioTransform):
             ymax = self.ymax
         else:
             ymax = np.max(ampenv)
+        if ymax == 0:
+            ymax = 1.0
 
         img = np.zeros(output_size)
         half_height = output_size[0] // 2
@@ -278,7 +280,7 @@ class AmplitudeEnvelopeTwoSidedTransform(AudioTransform):
             return _gradient_0 + ((i + 1) / half_height) * _gradient_span
 
         for col in range(len(ampenv)):
-            n_rows_to_fill = int(np.round(half_height * ampenv[col] / ymax))
+            n_rows_to_fill = int(np.round(scale * half_height * ampenv[col] / ymax))
             for i in range(n_rows_to_fill):
                 val = compute_color(i)
                 img[half_height + i, col] = val