13bc5a6b2d1042772641f1b51812e8ddb7c02132
Kevin Yu <thekevinyu@gmail.com>
2023-07-17 00:36:14 -0700

    use contextvar

--- src/inspec_curses/context.py
+++ src/inspec_curses/context.py
@@ -1,34 +1,43 @@
 from __future__ import annotations
+import contextvars
 
 import curses
-from typing import Optional
 
 from inspec_curses.color_pair import ColorToSlot
 
-_CURRENT_COLORMAP: Optional[ColorToSlot] = None
+_CURRENT_COLORMAP: contextvars.ContextVar[ColorToSlot] = contextvars.ContextVar("_CURRENT_COLORMAP")
 
 
 def get_active() -> ColorToSlot:
     """
     Get the currently active colormap
     """
-    if _CURRENT_COLORMAP is None:
+
+    current_colormap = _CURRENT_COLORMAP.get(default=None)
+    if current_colormap is None:
         raise RuntimeError(
             "Curses colormap not initialized; never called set_active_cmap(colors: ColorToSlot)"
         )
-    return _CURRENT_COLORMAP
+    return current_colormap
 
 
 def set_active(color_to_slot: ColorToSlot) -> ColorToSlot:
     """
     Apply the cmap to the current curses context.
     """
-    global _CURRENT_COLORMAP
-    for fg_color, bg_color, slot in color_to_slot.iter_color_pairs():
-        curses.init_pair(
-            slot.value,
-            fg_color.value,
-            bg_color.value,
-        )
-    _CURRENT_COLORMAP = color_to_slot
+
+    token = _CURRENT_COLORMAP.set(color_to_slot)
+    try:
+        for fg_color, bg_color, slot in color_to_slot.iter_color_pairs():
+            curses.init_pair(
+                slot.value,
+                fg_color.value,
+                bg_color.value,
+            )
+    except Exception as e:
+        _CURRENT_COLORMAP.reset(token)
+        raise RuntimeError(
+            "Unexpected error setting curses colormap"
+        ) from e
+
     return color_to_slot
